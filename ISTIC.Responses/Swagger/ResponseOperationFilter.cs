007c86c8fc2869bdea95d2194ecd9c111de4ef85
DiogoBotton <diogo.boton@sp.senai.br>
2024-09-27 16:25:47 -0300

    refactor: correct return on endpoints with empty response

--- ISTIC.Responses/Swagger/ResponseOperationFilter.cs
+++ ISTIC.Responses/Swagger/ResponseOperationFilter.cs
@@ -22,45 +22,70 @@ public class ResponseOperationFilter : IOperationFilter
             if (successReturnType.IsGenericType && successReturnType.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
                 return;
 
-            var successMediaType = new OpenApiMediaType
-            {
-                Schema = new OpenApiSchema
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Id = successReturnType.GetSchemaId(),
-                        Type = ReferenceType.Schema
-                    }
-                }
-            };
+            ConfigureResponses(operation, successReturnType);
+        }
+        else if (returnType == typeof(Response))
+            ConfigureResponses(operation, null);
+    }
 
-            var errorMediaType = new OpenApiMediaType
-            {
-                Schema = new OpenApiSchema
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Id = typeof(Error).GetSchemaId(),
-                        Type = ReferenceType.Schema
-                    }
-                }
-            };
+    private void ConfigureResponses(OpenApiOperation operation, Type successReturnType)
+    {
+        var errorMediaType = CreateErrorMediaType();
 
+        if (successReturnType != null)
+        {
+            var successMediaType = CreateSuccessMediaType(successReturnType);
             operation.Responses["200"] = new OpenApiResponse
             {
                 Content =
-                    {
-                        { "application/json",  successMediaType},
-                    }
-            };
-
-            operation.Responses["400"] = new OpenApiResponse
-            {
-                Content =
-                    {
-                        { "application/json", errorMediaType }
-                    }
+                {
+                    { "application/json", successMediaType }
+                }
             };
         }
+        else
+            operation.Responses["200"] = new OpenApiResponse
+            {
+                Description = "No content",
+                Content = new Dictionary<string, OpenApiMediaType>()
+            };
+
+        operation.Responses["400"] = new OpenApiResponse
+        {
+            Content =
+            {
+                { "application/json", errorMediaType }
+            }
+        };
+    }
+
+    private OpenApiMediaType CreateSuccessMediaType(Type successReturnType)
+    {
+        return new OpenApiMediaType
+        {
+            Schema = new OpenApiSchema
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = successReturnType.GetSchemaId(),
+                    Type = ReferenceType.Schema
+                }
+            }
+        };
+    }
+
+    private OpenApiMediaType CreateErrorMediaType()
+    {
+        return new OpenApiMediaType
+        {
+            Schema = new OpenApiSchema
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = typeof(Error).GetSchemaId(),
+                    Type = ReferenceType.Schema
+                }
+            }
+        };
     }
 }
