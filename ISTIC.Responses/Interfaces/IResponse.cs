e465c73b5e4d2d9ccd101c474fd422d3df53c93b
DiogoBotton <diogo.boton@sp.senai.br>
2024-10-03 15:28:29 -0300

    feat: add Error in IResponse interface

--- ISTIC.Responses/Interfaces/IResponse.cs
+++ ISTIC.Responses/Interfaces/IResponse.cs
@@ -1,8 +1,11 @@
-﻿using System.Net;
+﻿using ISTIC.Responses.Core;
+using System.Net;
 
 namespace ISTIC.Responses.Interfaces;
 
 public interface IResponse
 {
     public HttpStatusCode StatusCode { get; set; }
+
+    Error Error { get; set; }
 }
